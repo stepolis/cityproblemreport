c489a5bab85c25a78a94ed4ef4344da239e2055b
stepolis <stepolis@gmail.com>
2020-07-27 15:32:57 +0300

    Διορθώσεις στα Ελληνικά

--- reporter/forms.py
+++ reporter/forms.py
@@ -7,18 +7,30 @@ from django.contrib.auth.forms import *
 from django.contrib.auth.models import *
 from django.contrib.gis.db import models
 from django.contrib.gis import forms
+from django.utils.translation import gettext_lazy as _
 from bootstrap_datepicker_plus import *
 
 
 
 class NewUserForm(UserCreationForm):
 	email = forms.EmailField(required=True)
-	address = forms.PointField(widget=LeafletWidget())
+	address = forms.PointField(label='Διεύθυνση Κατοικίας', widget=LeafletWidget())
+	password1 = forms.CharField(label=_("Password"), widget=forms.PasswordInput, help_text=_("Τουλάχιστον 8 χαρακτήρες!	Όχι ίδιο με προσωπικά στοιχεία!	Όχι μόνο αριθμούς!	Όχι συνηθισμένα passwords!"))
+	password2 = forms.CharField(label=_("Επαλήθευση Password"), widget=forms.PasswordInput, help_text=_("Εισάγετε το ίδιο password, όπως πριν, για επαλήθευση!"))
 
 	class Meta:
 		model = User_Res
 		fields = ("email", "username", "password1", "password2", "address", "first_name",
 				  "last_name", "car_driver", "cellphone", "visitor", "visit_time")
+		labels = {
+			"first_name": _('Όνομα'), 
+			"last_name": _('Επίθετο'), 
+			"password2": _('Επαλήθευση Password'),
+			"username": _('Username (Όνομα Χρήστη)'),
+			}
+		help_texts = {
+			"username": 'Απαραίτητο! Επιτρέπονται Ελληνικά, Λατινικά, Αριθμοί και τα Σύμβολα: @/./+/-/_ μόνο.',
+		}
 		widgets = {
             "visit_time": DatePickerInput(format='%d/%m/%Y'),
             "address": LeafletWidget(),
@@ -37,7 +49,7 @@ class IncidentForm(ModelForm):
 
 	class Meta:
 		model = Incident
-		fields = ("address", "location", "date", "time", "support_indicator",
+		fields = ("location", "address", "date", "time", "support_indicator",
 				  "problem_kind", "user_username", "user_email", "status")
 		widgets = {
 			"location": LeafletWidget(),
